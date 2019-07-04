39da9a825e0b2a29ffef3bb68d3b9168e77e1ac2
Nikos Koutelidis <nikoutel@gmail.com>
2019-07-04 08:23:56 +0200

    Register System in service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\System\SystemInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,8 +12,8 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
-    {
-        //
+    public function register() {
+        $namespacedConcreteSystem = 'App\System\\' . php_uname('s');
+        $this->app->bind(SystemInterface::class, $namespacedConcreteSystem);
     }
 }
