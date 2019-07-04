5936b344b16a92c085d94dd3942d2757cd44fa9b
Nikos Koutelidis <nikoutel@gmail.com>
2019-07-04 08:27:46 +0200

    Create cpu routes

--- routes/web.php
+++ routes/web.php
@@ -25,31 +25,37 @@ $router->group(['prefix' => $prefix], function () use ($router) {
         return $router->app->version();
     }]);
 
+    $router->get('cpu', 'SystemController@cpuRoot');
+
+    $router->get('cpu/info', 'SystemController@cpuInfo');
+
+    $router->get('cpu/load', 'SystemController@cpuLoad');
+    
 });
 $router->get('/{route:.*}/', function (Request $request) {
-    return  getError(404, $request);
+    return getError(404, $request);
 });
 $router->post('/{route:.*}/', function (Request $request) {
-    return  getError(405, $request);
+    return getError(405, $request);
 });
 $router->put('/{route:.*}/', function (Request $request) {
-    return  getError(405, $request);
+    return getError(405, $request);
 });
 $router->delete('/{route:.*}/', function (Request $request) {
-    return  getError(405, $request);
+    return getError(405, $request);
 });
 $router->patch('/{route:.*}/', function (Request $request) {
-    return  getError(405, $request);
+    return getError(405, $request);
 });
 $router->options('/{route:.*}/', function (Request $request) {
-    return  getError(405, $request);
+    return getError(405, $request);
 });
 
-function getError($statusCode , Request $request) {
+function getError($statusCode, Request $request) {
     $url = $request->fullUrl();
     $method = $request->getRealMethod();
-    $errorTitle = array (404 => "Not Found" , 405 => "Method Not Allowed") ;
-    $message = array (404 => "Resource '$url' does not exist" , 405 =>  "'$method' method not allowed. Allowed methods: 'GET', 'HEAD'");
+    $errorTitle = array(404 => "Not Found", 405 => "Method Not Allowed");
+    $message = array(404 => "Resource '$url' does not exist", 405 => "'$method' method not allowed. Allowed methods: 'GET', 'HEAD'");
     $header = array(404 => array(), 405 => array('Allow' => 'GET, HEAD')); // For 'Content-Type' Header use ->header() not array parameter!!
     $error = array(
         "errors" => array(
